584a0c87d158e4c2e45ca1daef6e9a897e9d41a8
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-09-23 00:48:43 +0000

    fazendo formulário comunicar com php

--- dados.php
+++ dados.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Aprendendo PHP</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+<body>
+<div>
 <?php
 $valor = $_GET["v"];
 echo "Digitou $valor";
 ?>
+</div>
+</body>
+</html>
